dbd91ca94c50161696a02d76312ed57c0b91b1f6
Sayed M Rezaie <56697913+cdied@users.noreply.github.com>
2020-11-12 13:34:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time
-name = str(input("Enter you File Name >>> "))+".png"
+name = str(input("Enter you File Name >>> "))
 print("ScreenShot takes in 4 sec...")
 time.sleep(4)
 screen = pyautogui.screenshot()
-screen.save(name)
+screen.save(name+".png")
